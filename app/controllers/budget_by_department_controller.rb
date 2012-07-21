a6d12e45ba8e75d4aa2e33ef014309ffdd5bfdd6
Julian Tescher <virulent@gmail.com>
2012-07-21 16:17:55 -0700

    update to add rollup

---
+++ app/controllers/budget_by_department_controller.rb
@@ -0,0 +1,47 @@
+class BudgetByDepartmentController < ApplicationController
+  def index
+    query_string = <<-RAW_QUERY
+
+    // make sure we're using the right db; this is the same as "use aggdb;" in shell
+    db = db.getSiblingDB("hackoakbudget2");
+
+    // grouping
+    var g1 = db.runCommand(
+    { aggregate : "budget_line_items", pipeline : [
+        { $project : {
+    	revenue : 1,
+    	expenditure : 1,
+    	cost_center : 1,
+    	department : 1
+        }},
+        { $group : {
+    	_id : "$cost_center.name",
+    	line_item_count : { $sum : 1 },
+    	total_revenue : { $sum : "$revenue" },
+    	total_expenditure : { $max : "$expenditure" }
+        }}
+    ]});
+
+    var g2 = db.runCommand(
+    { aggregate : "budget_line_items", pipeline : [
+        { $project : {
+    	revenue : 1,
+    	expenditure : 1,
+    	cost_center : 1,
+    	department : 1
+        }},
+        { $group : {
+    	_id : "$department.name",
+    	line_item_count : { $sum : 1 },
+    	total_revenue : { $sum : "$revenue" },
+    	total_expenditure : { $max : "$expenditure" }
+        }}
+    ]});
+
+    return g2
+
+    RAW_QUERY
+
+    render :json => Mongoid.default_session.send(:current_database).command(:eval => query_string )['retval']['result']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HackTheBudget::Application.routes.draw do
   resources :home, only: :index
   resources :budget_line_items, only: :index
+  resources :budget_by_department, only: :index
   root to: 'home#index'
   match "/csv_import" => "csv#import"
   match "/csv_upload" => "csv#upload"
