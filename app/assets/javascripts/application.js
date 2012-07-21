75258474c743e354da7b2eb309ec8737c3f9b218
menslow <michael.enslow@gmail.com>
2012-07-21 13:12:46 -0700

    created hack_the_budget app.js file, removed extra jquery library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,55 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require app
 //= require_tree .
-
-
-var App = function(){
-
-	/* private */
-	var default_topic = 'Placeholder Text';
-
-	/* HTML5 elememt support tester */
-	var element_supports_attribute = function(element,attribute){
-		var test = document.createElement(element);
-		if (attribute in test) {
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	/* end private */
-
-	return {
-
-		init: function() {
-
-			if (!element_supports_attribute('input','placeholder')) {
-				App.add_focus_events();
-			}
-
-		},
-
-		add_focus_events: function(){
-
-			$('#foo').val(default_topic);
-
-			$('#foo').focus(function(){
-				if($(this).val()===default_topic){
-					$(this).val('');
-				}
-			});
-			$('#foo').blur(function(){
-				if($(this).val()===''){
-					$(this).val(default_topic);
-				}
-			});
-		}
-
-	};
-}();
-
-$(document).ready(function($){
-	App.init();
-});
