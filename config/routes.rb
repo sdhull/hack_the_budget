cd1bc11a00071af1628e22c17a4f1829317b7d28
Patrick Tescher <pat2man@gmail.com>
2012-07-21 14:55:41 -0700

    Merged models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HackTheBudget::Application.routes.draw do
   resources :home, only: :index
   root to: 'home#index'
+  match "/csv_import" => "csv#import"
+  match "/csv_upload" => "csv#upload"
 end
