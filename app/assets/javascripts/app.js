c32ed70c4c60dde5036fbe02e2609fda3da4ded0
menslow <michael.enslow@gmail.com>
2012-07-21 15:17:39 -0700

    getting ready for json for main pie

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,18 +12,45 @@ var App = function(){
   }
   
   var pieClick = function(event, pos, obj) {
-    alert("click");
+    //console.log("click");
   }
 
-  /* HTML5 elememt support tester */
-  var element_supports_attribute = function(element,attribute){
-    var test = document.createElement(element);
-    if (attribute in test) {
-      return true;
-    } else {
-      return false;
+  var load_general_fund_pie = function() {
+
+    var data = [];
+    var series = Math.floor(Math.random()*10)+1;
+    for( var i = 0; i<series; i++)
+    {
+      data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random() * 100)+1 }
     }
-  };
+    
+    // DEFAULT
+    $.plot($("#pie"), data, {
+        series: {
+          pie: { 
+            show: true
+          }
+        },
+        grid: {
+          hoverable: true,
+          clickable: true
+        }
+    });
+    /*
+        $.ajax({url: "/path_to_chart_data",
+          data: {
+           tag: tag
+          },
+          success: function(data) {
+            chart_data = $.parseJSON(data);
+            $.plot($("#pie"), chart_data);
+          },
+          error: function(data) {
+            console.log("Error loading chart data");
+          }
+        })
+    */
+  }
 
   /* end private */
 
@@ -31,73 +58,19 @@ var App = function(){
 
     init: function() {
 
-      if (element_supports_attribute('input','placeholder')) {
-        App.add_focus_events();
-      }
       App.add_some_pie();
 
     },
 
-    add_focus_events: function(){
-
-      $('#foo').val(default_topic);
-
-      $('#foo').focus(function(){
-        if($(this).val()===default_topic){
-          $(this).val('');
-        }
-      });
-      $('#foo').blur(function(){
-        if($(this).val()===''){
-          $(this).val(default_topic);
-        }
-      });
-    },
-    
     add_some_pie: function(){
 
-      var data = [];
-      var series = Math.floor(Math.random()*10)+1;
-      for( var i = 0; i<series; i++)
-      {
-        data[i] = { label: "Series"+(i+1), data: Math.floor(Math.random() * 100)+1 }
-      }
-      
-      // DEFAULT
-      $.plot($("#pie"), data, {
-          series: {
-            pie: { 
-              show: true
-            }
-          },
-          grid: {
-            hoverable: true,
-            clickable: true
-          }
-      });
-
+      load_general_fund_pie();
       $("#pie").bind("plothover", pieHover);
       $("#pie").bind("plotclick", pieClick);
 
-    },
-  
-    // Load and set the chart data to our ajax method
-    load_chart_data: function(tag) {
-	  $.ajax({
-	    url: "/path_to_chart_data",    
-	      data: {
-	       tag: tag
-	      },
-	      success: function(data) {
-	        chart_data = $.parseJSON(data);
-	        $.plot($("#pie"), chart_data);
-	      },
-	      error: function(data) {
-	        alert("Error loading chart data");
-	      }
-	  })
     }
   }
+
 }();
 
 $(document).ready(function($){
