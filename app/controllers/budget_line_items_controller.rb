820ec7a8cdb6ee1253a0981dcb8006190a1ba17f
Tom Fite <tomfite@gmail.com>
2012-07-28 14:48:14 -0700

    Trying to sort budget by expenditure

--- app/controllers/budget_line_items_controller.rb
+++ app/controllers/budget_line_items_controller.rb
@@ -2,6 +2,10 @@ class BudgetLineItemsController < ApplicationController
   respond_to :json
   def index
     @budget_line_items = BudgetLineItem.all
+    @budget_line_items.sort do |a|
+      [a.expenditure]
+    end
+    
     respond_with @budget_line_items
   end
 end
